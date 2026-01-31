43e62683cc13d037d9b99fb69d823724399697dd
aaryan <aaryankalia165@gmail.com>
2026-01-31 09:28:18 +0530

    Solved Phase1 -> Level1 -> Q7 (Largest Number between 3 Numbers)

---
+++ 1. Logic Building/Phase 1 – Conditional Thinking/Level 1 Simple Conditions/Q7LargestOfThree.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class Q7LargestOfThree
+{
+    public static void main(String[] args)
+    {
+        try(Scanner sc = new Scanner(System.in))
+        {
+            System.out.println("Enter your 1st number: ");
+            int num1 = sc.nextInt();
+
+            System.out.println("Enter your 2nd number: ");
+            int num2 = sc.nextInt();
+
+            System.out.println("Enter your 3rd number: ");
+            int num3 = sc.nextInt();
+
+            if (num1 > num2 && num1 > num3)
+            {
+                System.out.println(num1 + "is the Largest Number");
+            }
+
+            else if(num2 > num1 && num2 > num3)
+            {
+                System.out.println(num2 + " is the Largest Number");
+            }
+
+            else
+            {
+                System.out.println(num3 + " is the Largest Number");
+            }
+        }
+    }
+}
