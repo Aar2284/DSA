93c621fb43ce8f3380800537d70e3cab8e88c8ba
aaryan <aaryankalia165@gmail.com>
2026-01-31 09:57:41 +0530

    Solved Phase1 -> Level1 -> Q8 (Alphabet is Vowel or Consonant)

---
+++ 1. Logic Building/Phase 1 – Conditional Thinking/Level 1 Simple Conditions/Q9VowelOrConsonant.java
@@ -0,0 +1,26 @@
+// Take a character and check if it’s a vowel or consonant
+
+import java.util.Scanner;
+
+public class Q9VowelOrConsonant
+{
+    public static void main(String[] args)
+    {
+        try(Scanner sc = new Scanner(System.in))
+        {
+            System.out.println("Enter an alphabet: ");
+            char ch = sc.next().charAt(0);
+
+            // Check if the character is a vowel
+            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+                ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                System.out.println(ch + " is a Vowel.");
+            }
+            else
+            {
+                System.out.println(ch + " is a Consonant.");
+            }
+        }
+    }
+}
