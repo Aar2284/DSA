2317f1d68e95e819a007b499be8dacbb5e48ecc8
aaryan <aaryankalia165@gmail.com>
2026-02-02 21:23:24 +0530

    Solved Phase1 -> Level3 -> Q3 (Check if the First and Last digits are Equal of a 4-Digit Number)

--- 1. Logic Building/Phase 1 – Conditional Thinking/Level 3 Math and Number Logic/Q3FirstLastDigitEqual.java
+++ 1. Logic Building/Phase 1 – Conditional Thinking/Level 3 Math and Number Logic/Q3FirstLastDigitEqual.java
@@ -1,4 +1,4 @@
-// Take a 4-digit number and check if the first and last digits are equal. 
+// Take a 4-digit number and check if the first and last digits are equal.
 
 import java.util.Scanner;
 
